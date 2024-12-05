b03d171575ca9c27ef30931cf8220b7c918ec87e
mounikapujari <mounikapujari2005@gmail.com>
2024-12-05 16:22:43 +0530

    Add files via upload

---
+++ increanddecre.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+public class increanddecre{
+    public static void main(String args[]){
+        int n=5;
+        System.out.println(n++);
+        int a=6;
+        System.out.println(++a);
+        int b=78;
+        System.out.println(--a);
+        System.out.println(a--);
+    }
+}
