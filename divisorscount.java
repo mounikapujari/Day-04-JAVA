892a14643fc377518bf5237b0f5d6f69c993838d
mounikapujari <mounikapujari2005@gmail.com>
2024-12-05 14:33:41 +0530

    Add files via upload

---
+++ divisorscount.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class divisorscount{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int n=s.nextInt();
+            int count=0;
+            for(int j=1;j<=n;j++)
+            {
+                if(n%j==0)
+                {
+                    System.out.print(j+" ");
+                    count+=1;
+                }
+            }
+            System.out.println("count="+count);
+            System.out.println();
+        }
+    }
+}
